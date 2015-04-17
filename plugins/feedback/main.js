26ec4109aff0c26861e0bab95fa79d2143933920
okwuokenye <okwuokenye@gmal.com>
2015-04-17 14:01:04 +0100

    fixing bug with feedback plugin

--- plugins/feedback/main.js
+++ plugins/feedback/main.js
@@ -9,7 +9,7 @@ define(templates, function(theme, feedbackForm) {
             name: "feedback",
             type: "general",
             menuURL: "#feedback",
-            icon: "plugins/myfiles/icon.png",
+            icon: "plugins/events/icon.png",
             lang: {
                 component: "core"
             }
@@ -18,7 +18,17 @@ define(templates, function(theme, feedbackForm) {
             ["feedback", "feedback_form", "loadFeedback"]
         ],
         loadFeedback: function() {
-            var tpl = {};
+            var tpl = {
+                user: {
+                    fullname: MM.site.get('fullname'),
+                    profileimageurl: MM.site.get('userpictureurl')
+                },
+                siteurl: MM.site.get('siteurl'),
+                coursePlugins: coursePlugins,
+                courses: courses,
+                plugins: plugins,
+                docsurl: docsurl
+            };
             var html = MM.tpl.render(feedbackForm, tpl);
             MM.panels.show('center', html, {title: MM.lang.s("feedbackHeader")});
         }
